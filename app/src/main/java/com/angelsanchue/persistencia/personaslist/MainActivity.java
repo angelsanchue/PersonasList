63ba05269594f33387deeea4e6996872c7ba108b
angelsanchue <44126119+angelsanchue@users.noreply.github.com>
2018-10-16 18:06:44 -0500

    Pruebad

--- app/src/main/java/com/angelsanchue/persistencia/personaslist/MainActivity.java
+++ app/src/main/java/com/angelsanchue/persistencia/personaslist/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Prueba de sincronizacion
+        //Prueba de sincronizacion otro
     }
 }
